a925ff9ccfc3a3d99be8ff5e5ef1c5754cbd36c0
jsgonzalez15b <47197962+jsgonzalez15b@users.noreply.github.com>
2019-04-26 13:48:50 -0500

    Create Arco.java
    Creacion de arco, por ahora terminado

---
+++ src/model/data_structures/Arco.java
@@ -0,0 +1,47 @@
+package model.data_structures;
+
+/*
+ * Clase que representa el arco, almacena el id V generico del vertice adyacente y su peso A generico
+ */
+public class Arco<K extends Comparable<K>,A>
+{
+
+	//Atributos
+	/**
+	 * Id generico del segundo vertice
+	 */
+	private K llaveId;
+
+	/**
+	 * Peso del arco
+	 */
+	private A pesoArco;
+	
+	//Constructor
+	/**
+	 * @param pPeso peso del arco
+	 * @param pLlave llave id del vertice adyacente
+	 */
+	public Arco (A pPeso,K pLlave)
+	{
+		//se inicializan los atributos del arco
+		llaveId=pLlave;
+		pesoArco=pPeso;
+	}
+	//Metodos
+	/**
+	 * Retorna el Id del vertice adyacente
+	 */
+	public K darAdyacente()
+	{
+		return llaveId;
+	}
+	
+	/**
+	 * Retorna el Peso del arco
+	 */
+	public A darPeso()
+	{
+		return pesoArco;
+	}
+}
